a3393a0ae23284003cf5a6718505c79883825615
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-03-20 19:25:48 +0300

    Add files via upload

---
+++ MP_Lab4/LCS/LCSMain.cpp
@@ -0,0 +1,27 @@
+﻿#include <tchar.h>
+#include <iostream>
+#include "LCS.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    setlocale(LC_ALL, "rus");
+
+    char X[] = "Кон", Y[] = "Локон";
+
+    cout << endl << "-- вычисление длины LCS для X и Y(рекурсия)";
+    cout << endl << "-- последовательность X: " << X;
+    cout << endl << "-- последовательность Y: " << Y;
+
+    int s = lcs(
+        sizeof(X) - 1,  // длина последовательности X   
+        X,              // последовательность X
+        sizeof(Y) - 1,  // длина последовательности Y
+        Y               // последовательность Y
+    );
+
+    cout << endl << "-- длина LCS: " << s << endl;
+    system("pause");
+    return 0;
+}
---
+++ MP_Lab4/Matrix/Matrix.cpp
@@ -0,0 +1,62 @@
+﻿#include "stdafx.h"
+#include <cmath>
+#include <memory.h>
+#include <iostream>
+#include "Multymatrix.h"   // умножение матриц 
+
+#define N 6
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int Mc[N + 1] = { 10,52,100,33,50,40,21 }, Ms[N][N], r = 0, rd = 0;
+
+	memset(Ms, 0, sizeof(int) * N * N);
+	r = OptimalM(1, N, N, Mc, OPTIMALM_PARM(Ms));
+
+	cout << endl << "-- расстановка скобок (рекурсивное решение) "<< endl;
+	cout << endl << "размерности матриц: ";
+
+	for (int i = 1; i <= N; i++) 
+		cout << "(" << Mc[i - 1] << "," << Mc[i] << ") ";
+
+	cout << endl << "минимальное количество операций умножения: " << r;
+	cout << endl << endl << "матрица S" << endl;
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << std::endl;
+
+		for (int j = 0; j < N; j++)  
+			cout << Ms[i][j] << "  ";
+	}
+	cout << endl;
+
+	memset(Ms, 0, sizeof(int) * N * N);
+	rd = OptimalMD(N, Mc, OPTIMALM_PARM(Ms));
+
+	cout << endl << "-- расстановка скобок (динамичеое программирование) " << endl;
+	cout << endl << "размерности матриц: ";
+
+	for (int i = 1; i <= N; i++)
+		cout << "(" << Mc[i - 1] << "," << Mc[i] << ") ";
+
+	cout << endl << "минимальное количество операций умножения: " << rd;
+	cout << endl << endl << "матрица S" << endl;
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << endl;
+
+		for (int j = 0; j < N; j++)  
+			cout << Ms[i][j] << "  ";
+	}
+
+	cout << endl << endl;
+	system("pause");
+
+	return 0;
+}
